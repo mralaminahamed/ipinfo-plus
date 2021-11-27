f068b3d5f8ad89b0d2d2ce4bbc359ba691d1d290
Al-Amin Ahamed <alamin@mishusoft.com>
2021-11-27 08:46:14 +0600

    update

--- src/assets/typescript/ipinfo.ts
+++ src/assets/typescript/ipinfo.ts
@@ -2,6 +2,6 @@
  * IP Info Plus
  * Developer: Al-Amin Ahamed
  * Website: https://www.mishusoft.com
- * Home: https://mishusoft.com/brower/addons/ipinfoplus/
+ * Home: https://mishusoft.com/browser/addons/ipinfoplus/
  * license : GPL-3.0-only
  * */
